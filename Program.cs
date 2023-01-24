bc22793efd012f7099b43ac5f4ee14554c4090cc
Maxim Shmidt <history1266@mail.ru>
2023-01-24 10:45:56 +0700

    добавил код с решением задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿Console.Clear();
 string[] array = Console.ReadLine().Split(" ").Select(x =>Convert.ToString(x)).ToArray();
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
